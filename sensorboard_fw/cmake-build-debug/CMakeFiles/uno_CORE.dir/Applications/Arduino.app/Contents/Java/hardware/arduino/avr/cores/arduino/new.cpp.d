2a4a72aeb79028e6c1906fbb8d5edb40238b3c7d
Yaakov Azat <yaakovazat@gmail.com>
2021-09-06 13:24:09 +0600

    latest from imac

--- sensorboard_fw/cmake-build-debug/CMakeFiles/uno_CORE.dir/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/cores/arduino/new.cpp.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/uno_CORE.dir/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/cores/arduino/new.cpp.obj: \
- /Applications/Arduino.app/Contents/Java/hardware/arduino/avr/cores/arduino/new.cpp
